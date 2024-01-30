43dcc24dd8e5ea6948d775d933b4ee0d64c67c26
Gruunegao <dalarius_romualdo@outlook.com>
2024-01-30 00:39:51 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -3,21 +3,59 @@ const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm){
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `https://65b85c7046324d531d56210c.mockapi.io/artists?name=${searchTerm}`
     //procurar apenas o que o usuario digito e toda informacao 
     fetch(url) // fazer requisicoes de api
+    .catch((error) => { 
+        console.log(error);
+    })
         .then((response) => response.json())
         .then((result) => displayResults(result))
 };
 
 function displayResults(results) {
     resultPlaylist.classList.add('hidden');
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    const artistsList = document.getElementById('artists-result-list');
 
     results.forEach(element => {
+        const artistCard = document.createElement('div');
+        artistCard.classList.add('artist-card');
+        artistCard.id = element.id;
+        
+        const cardImg = document.createElement('div');
+        cardImg.classList.add('card-img');
+        const artistImg = document.createElement('img');
+        artistImg.classList.add('artist-img');
+        artistImg.src = element.urlImg;
+        cardImg.appendChild(artistImg);
+        
+        const play = document.createElement('div');
+        play.classList.add('play');
+        const faPlay = document.createElement('span');
+        faPlay.classList.add('fa', 'fa-solid', 'fa-play');
+        play.appendChild(faPlay);
+        cardImg.appendChild(play);
+        artistCard.appendChild(cardImg);
+
+        const cardText = document.createElement('div');
+        cardText.classList.add('card-text');
+        const a = document.createElement('a');
+        a.classList.add('vst');
+        a.href = '';
+        a.title = element.name;
+        const artistName = document.createElement('span');
+        artistName.classList.add('artist-name');
+        artistName.id = 'artist-name';
         artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
+        
+        const artistCategorie = document.createElement('span');
+        artistCategorie.classList.add('artist-categorie');
+        artistCategorie.innerText = 'Artista';
+        cardText.appendChild(a);
+        cardText.appendChild(artistName);
+        cardText.appendChild(artistCategorie);
+        artistCard.appendChild(cardText);
+        artistsList.appendChild(artistCard);
     });
 
     resultArtist.classList.remove('hidden');
@@ -27,11 +65,15 @@ function displayResults(results) {
 
 document.addEventListener('input', function(){
     const searchTerm = searchInput.value.toLowerCase();
+    (document.getElementById('artists-result-list')).innerHTML = ''; // limpa a lista de artistas
+
     if(searchTerm === ''){
-        resultPlaylist.classList.add('hidden');
+        resultPlaylist.classList.remove('hidden');
         resultArtist.classList.remove('hidden');
         return;
     }
+
+
 
     requestApi(searchTerm);
 });
